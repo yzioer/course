ff68d7c6882e891b4e668a1da42cce756b696c2f
Luzius Meisser <luzius.meisser+git@gmail.com>
2017-09-18 17:48:10 +0200

    added latest version of exercise 1

--- arena/src/com/agentecon/web/SimulationServer.java
+++ arena/src/com/agentecon/web/SimulationServer.java
@@ -40,6 +40,7 @@ public class SimulationServer extends VisServer {
 		try {
 			this.simulations.add(new GitSimulationHandle("meisser", "course", "master"));
 			this.simulations.add(new GitSimulationHandle("meisser", "course", "ex1-hermit"));
+			this.simulations.add(new GitSimulationHandle("meisser", "course", "ex1-hermit-0"));
 			this.simulations.add(new GitSimulationHandle("meisserecon", "agentecon", "demo"));
 		} catch (IOException e) {
 			System.out.println("Disabled remote repositories. " + e.getMessage());
