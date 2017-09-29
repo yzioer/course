75f3a193b34b200c57b61efb1d54ac241b0ef9d7
Luzius Meisser <luzius.meisser+git@gmail.com>
2017-09-29 12:46:17 +0200

    making farming configuration the default again

--- simulation/src/com/agentecon/Simulation.java
+++ simulation/src/com/agentecon/Simulation.java
@@ -12,7 +12,6 @@ import com.agentecon.agent.IAgents;
 import com.agentecon.configuration.IConfiguration;
 import com.agentecon.events.SimEvent;
 import com.agentecon.exercises.FarmingConfiguration;
-import com.agentecon.exercises.HermitConfiguration;
 import com.agentecon.finance.StockMarket;
 import com.agentecon.goods.Good;
 import com.agentecon.market.IDiscountRate;
@@ -44,7 +43,7 @@ public class Simulation implements ISimulation, IStatistics, IIteratedSimulation
 	private MarketStatistics goodsMarketStats;
 
 	public Simulation() throws IOException {
-		this(new HermitConfiguration());
+		this(new FarmingConfiguration());
 	}
 	
 	public Simulation(IConfiguration metaConfig) {
